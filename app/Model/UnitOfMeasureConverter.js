8e2bbc48f31674df168e590c7ed289702c54f80e
cekator3 <hamsterdreams@inbox.ru>
2023-10-30 22:21:55 +0500

    Added implementation for UnitOfMeasureConverter.js

--- app/Model/UnitOfMeasureConverter.js
+++ app/Model/UnitOfMeasureConverter.js
@@ -2,12 +2,15 @@
  * @file A subsystem for converting a value from one unit of measurement to another
  */
 
+import {getMultiplierForConvertingValueFromOneUnitOfMeasureToAnother} from "~/Model/UnitsOfMeasureConvertionMultipliers";
+
+export class ConvertationNotImplementedException extends Error {}
 
 /**
- * Converts a value from one unit of measurement to another
- * @param value
- * @param currentUnitOfMeasurement {string} Current unit of measurement of the given value.
- * @param targetUnitOfMeasurement {string} Target unit of measurement.
+ * Converts a number from one unit of measurement to another
+ * @param {number} value
+ * @param {string} currentUnitOfMeasurement Current unit of measurement of the given value.
+ * @param {string} targetUnitOfMeasurement Target unit of measurement.
  * @returns {number}
  * @constructor
  */
@@ -20,9 +23,7 @@ export function ConvertValueToAnotherUnitOfMeasurement(value,
         currentUnitOfMeasurement,
         targetUnitOfMeasurement
     );
-    //Есть две группы величин:
-    // 1. которые конвертируются при помощи одной арифметической операции (время)
-    // 2. в которых нужно использовать формулу (температура)
-
-    return value ;
+    if (multiplier === undefined)
+        throw new ConvertationNotImplementedException();
+    return value * multiplier;
 }
