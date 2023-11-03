b9507c799c4160b98b56afa09d5743307567a1d0
cekator3 <hamsterdreams@inbox.ru>
2023-11-03 15:50:16 +0500

    some stuff

--- app/Model/ListPicker.js
+++ app/Model/ListPicker.js
@@ -1,35 +1,89 @@
 /**
- * @file A subsystem for storing a list and its selected value
+ * @file A subsystem for storing a list of unique items and its selected item
  */
+
+let set = undefined;
+let selectedElement = undefined;
+
+export class ListPickerNotInitiliasedException extends Error {}
+export class ListPickerGivenNonUniqueItems extends Error
+{
+    items;
+    constructor(items)
+    {
+        super('Given non unique items in ListPicker: ' + items);
+        this.items = items;
+    }
+}
+export class ElementIsNotInListPickerException extends Error
+{
+    element;
+    listPickerContents;
+    constructor(element, listPickerContents)
+    {
+        super('Element ' + element + ' is not in ListPicker ' + listPickerContents);
+        this.element = element;
+        this.listPickerContents = listPickerContents;
+    }
+}
 
 /**
  * Sets the contents of the list.
- * @param {string[]} elementsIdentifiers
+ * @param {string[]} elementsIdentifiers Must be unique values
  * @return {void}
  */
-export function SetListElements(elementsIdentifiers) {}
+export function ListPickerSetElements(elementsIdentifiers)
+{
+    set = new Set(elementsIdentifiers);
+    if (set.size !== elementsIdentifiers.length)
+        throw new ListPickerGivenNonUniqueItems(elementsIdentifiers);
+    selectedElement = elementsIdentifiers[0];
+}
 
 /**
  * Sets the selected item in the list
  * @param {string} elementIdentifier
  * @return {void}
  */
-export function SetSelectedElement(element) {}
+export function ListPickerSetSelectedElement(elementIdentifier)
+{
+    if (set === undefined)
+        throw new ListPickerNotInitiliasedException();
+    if (!set.has(elementIdentifier))
+        throw new ElementIsNotInListPickerException(elementIdentifier, [...set]);
+    selectedElement = elementIdentifier;
+}
 
 /**
  * Returns the selected item of the list
  * @return {string}
  */
-export function GetSelectedElement() {}
+export function ListPickerGetSelectedElement()
+{
+    return selectedElement;
+}
 
 /**
  * Returns all non-selected items of the list
  * @return {string[]}
  */
-export function GetNonSelectedElements() {}
+export function ListPickerGetNonSelectedElements()
+{
+    if (set === undefined)
+        return [];
+    set.delete(selectedElement);
+    let result = [...set];
+    set.add(selectedElement);
+    return result;
+}
 
 /**
  * Returns all items of the list
  * @return {string[]}
  */
-export function GetAllElements() {}
+export function ListPickerGetAllElements()
+{
+    if (set === undefined)
+        return [];
+    return [...set];
+}
---
+++ app/tests/ListPicker.spec.js
@@ -0,0 +1,44 @@
+import
+{
+    ListPickerSetElements,
+    ListPickerSetSelectedElement,
+    ListPickerGetSelectedElement,
+    ListPickerGetNonSelectedElements,
+    ListPickerGetAllElements
+}
+from "~/Model/ListPicker";
+
+describe('ListPicker stores all given elements.', testListPickerStoresAllGivenElements);
+describe('ListPicker stores only unique items.', testListPickerStoresOnlyUniqueItems);
+describe('ListPicker returns non-selected items correctly.', testListPickerReturnsNonSelectedItemsCorrectly);
+
+function testListPickerStoresAllGivenElements()
+{
+    let list = [1, 2, 3, 4, 5, 'Hello'];
+    it(list + ' must be stored in ListPicker ', function ()
+    {
+        ListPickerSetElements(list);
+        expect(ListPickerGetAllElements()).toEqual(list);
+    });
+}
+
+function testListPickerStoresOnlyUniqueItems()
+{
+    let list = [1, 2, 2, 3, 4, 5, 'Hello'];
+    it(list + ' must throw an exception because ListPicker contains only unique items', function ()
+    {
+        expect(function() { ListPickerSetElements(list); }).toThrow();
+    });
+}
+
+function testListPickerReturnsNonSelectedItemsCorrectly()
+{
+    let list = [1, 2, 3, 4, 5, 'Hello'];
+    let selectedItem = list.at(-1);
+    it('Selected item from list ' + list + ' must be ' + selectedItem, function ()
+    {
+        ListPickerSetElements(list);
+        ListPickerSetSelectedElement(selectedItem);
+        expect(ListPickerGetNonSelectedElements()).toEqual(list.slice(0, -1));
+    });
+}
